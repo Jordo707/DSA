210dea3f1a3ef2a8b0caefe77fbe5dac66c9bbe4
Jordan <penttijordo@gmail.com>
2024-08-09 13:50:10 -0400

    pivotInteger

---
+++ August-24/pivotInteger.js
@@ -0,0 +1,24 @@
+/*
+
+Given a positive integer n, find the pivot integer x such that:
+
+The sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively.
+
+Return the pivot integer x. If no such integer exists, return -1. It is guaranteed that there will be at most one pivot index for the given input.
+
+*/
+
+const pivotInteger = n => {
+    // Find the square root of the sum formula for n
+    const res = Math.sqrt(n * (n + 1) / 2)
+
+    // Ensure the square root is an integer, returning -1 if it ist't
+    if (res % 1) return -1
+
+    return res
+}
+
+// Test cases
+console.log(pivotInteger(8)) // Expect 6
+console.log(pivotInteger(1)) // Expect 1
+console.log(pivotInteger(4)) // Expect -1
