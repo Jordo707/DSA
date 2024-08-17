b5d2718bf914a5a5e843709be9c8c81ade674479
Jordan <penttijordo@gmail.com>
2024-08-16 21:20:47 -0400

    powXN

---
+++ August-24/powXN.js
@@ -0,0 +1,12 @@
+/*
+
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+*/
+
+const myPow = (x, n) => x ** n
+
+// Test Cases
+console.log(myPow(2, 10)); // 1024
+console.log(myPow(2.1, 3)); // 9.261
+console.log(myPow(2, -2)); // .25
