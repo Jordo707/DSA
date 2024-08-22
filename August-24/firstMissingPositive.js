aaa8a0d16b2b361a0402c194ee60404adb452cab
Jordan <penttijordo@gmail.com>
2024-08-22 13:59:29 -0400

    firstMissingPositive.js

--- August-24/firstMissingPositive.js
+++ August-24/firstMissingPositive.js
@@ -8,9 +8,22 @@ You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary s
 
 const firstMissingPositive = nums => {
 
+    for (let i = 0; i < nums.length; i++) {
+        while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] -1] !== nums[i]) {
+            [nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]];
+        }
+    }
 
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== i + 1) {
+            return i + 1
+        }
+    }
 
+    return nums.length + 1
 }
 
 // Test Cases
-console.log(object)
+console.log(firstMissingPositive([1,2,0])); // 3
+console.log(firstMissingPositive([3,4,-1,1])); // 2
+console.log(firstMissingPositive([7,8,9,11,12])); // 1
